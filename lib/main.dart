a874a6443da24874d3acc95c5b4d182c205aa32b
A. Lester Buck III <buck@compact.com>
2024-03-27 19:19:16 -0500

    Changed start values back to normal

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _delay = 500;
+  int _delay = 5;
   int _maxtul = 150;
   int _countdown = 0;
   int _countup = 0;
